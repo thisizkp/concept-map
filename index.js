2b59a9a4aa9a847f5d86de29393ed7a0d5b69e7f
Prasanth Karri <prasanth.25k@gmail.com>
2018-06-09 17:59:59 +0530

    add click event listener

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const selectPrimaryNode = nodesList => {
+const getRandomNode = nodesList => {
   const keys = Object.keys(nodesList);
   const randomKey = keys[Math.floor(Math.random() * keys.length)];
   return nodesList[randomKey];
 }
 
-const getAdjacentNodes = (primaryNode, relations) => {  
+const getAdjacentNodes = primaryNode => {
   return relations[primaryNode.EID].sort((a,b) => {
     if (a.score < b.score) return 1;
     else if (a.score > b.score) return -1;
@@ -27,51 +27,69 @@ const drawCircle = (x, y, r) => {
   circle.setAttribute("cx", x);
   circle.setAttribute("cy", y);
   circle.setAttribute("r", r);
-  circle.setAttribute("fill", "#81ecec");
+  circle.setAttribute("fill", "#54a0ff");
+  circle.setAttribute("class", "circle");
+  circle.onclick = function(e) {
+    const id = e.target.closest("g").id;
+    drawMap(subjects[id]);
+  }
   return circle;
 }
 
-const drawNode = (subjects, node, x, y, r) => {
+const drawNode = (node, x, y, r) => {
   const group = document.createElementNS("http://www.w3.org/2000/svg", "g");
+  group.setAttribute("class", "node");
+  group.setAttribute("id", node.EID);
   group.appendChild(drawCircle(x, y, r));
   group.appendChild(drawText(subjects[node.EID].name, x, y));
-  group.setAttribute("id", node.EID)
   return group;
 }
 
-const drawLine = (x, y) => {
+const drawLine = (x1, y1, x2, y2) => {
   const line = document.createElementNS("http://www.w3.org/2000/svg", "line");
-  line.setAttribute("x1", 85);
-  line.setAttribute("y1", 85);
-  line.setAttribute("x2", x);
-  line.setAttribute("y2", y);
-  line.setAttribute("stroke", "black");
+  line.setAttribute("x1", x1);
+  line.setAttribute("y1", y1);
+  line.setAttribute("x2", x2);
+  line.setAttribute("y2", y2);
+  line.setAttribute("stroke", "#00d2d3");
   line.setAttribute("stroke-width", 0.8);
   return line;
 }
 
-const drawRelation = (node, x, y) => {
+const drawRelation = (node, x1, y1, x2, y2) => {
   const group = document.createElementNS("http://www.w3.org/2000/svg", "g");
-  group.appendChild(drawLine(x, y));
-  group.appendChild(drawText(node.score, x, y));
+  group.setAttribute("class", "relation");
+  group.appendChild(drawLine(x1, y1, x2, y2));
+  group.appendChild(drawText(node.score, x2, y2));
   return group;
 }
 
-const drawMap = (subjects, relations) => {
-  const primaryNode = selectPrimaryNode(subjects);
+const drawMap = primaryNode => {
   const svg = document.getElementById("svg");
-  svg.appendChild(drawNode(subjects, primaryNode, 100, 100, 20));
-
-  const adjacentNodes = getAdjacentNodes(primaryNode, relations);
-  const x = [20, 180, 20, 180]; const y = [20, 20, 180, 180];
-  for(let [index, node] of adjacentNodes.entries()) {
-    svg.appendChild(drawRelation(node, x[index], y[index]));
-    svg.appendChild(drawNode(subjects, node, x[index], y[index], 20));
+  while(svg.lastChild) {
+    svg.removeChild(svg.lastChild);
   }
+
+  // TODO: refactor dimensions logic
+  svg.appendChild(drawNode(primaryNode, 150, 150, 20));
+  const x = [60, 240, 60, 240]; const y = [60, 60, 240, 240];
+  const x1 = [135, 165, 135, 165]; const y1 = [135, 135, 165, 165];
+
+  const adjacentNodes = getAdjacentNodes(primaryNode);
+  for(let [index, node] of adjacentNodes.entries()) {
+    svg.appendChild(drawRelation(node, x1[index], y1[index], x[index], y[index]));
+    svg.appendChild(drawNode(node, x[index], y[index], 10));
+  }
+}
+
+const startApp = (subjects, relations) => {
+  window.subjects = subjects;
+  window.relations = relations;
+  drawMap(getRandomNode(subjects));
 }
 
 const fetchSubjects = fetch('./data/subjects.json').then(response => response.json());
 const fetchRelations = fetch('./data/related.json').then(response => response.json());
 Promise.all([fetchSubjects, fetchRelations]).then(([subjects, relations]) => {
-  drawMap(subjects, relations);
+  startApp(subjects, relations);
 })
