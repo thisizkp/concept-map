1f11e8ce9b958903fada8a48b7b2a6961dc04316
Prasanth Karri <prasanth.25k@gmail.com>
2018-06-09 11:42:43 +0530

    get primary & adjacent nodes

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
-console.log("setup done");
+const selectPrimaryNode = nodesList => {
+  const keys = Object.keys(nodesList);
+  const randomKey = keys[Math.floor(Math.random() * keys.length)];
+  return nodesList[randomKey];
+}
 
-fetch('./data/subjects.json')
-  .then(function(response) {
-    return response.json()
-  })
-  .then(function(data) {
-    console.log(data);
-  })
+const getAdjacentNodes = (primaryNode, relations) => {
+  return relations[primaryNode.EID].sort((a,b) => {
+    if (a.score < b.score) return 1;
+    else if (a.score > b.score) return -1;
+    return 0;
+  }).slice(0,4);
+}
 
-fetch('./data/related.json')
-  .then(function(response) {
-    return response.json()
-  })
-  .then(function(data) {
-    console.log(data);
-  })
+const fetchSubjects = fetch('./data/subjects.json').then(response => response.json());
+const fetchRelations = fetch('./data/related.json').then(response => response.json());
+Promise.all([fetchSubjects, fetchRelations]).then(([subjects, relations]) => {
+  const primaryNode = selectPrimaryNode(subjects);
+  const adjacentNodes = getAdjacentNodes(primaryNode, relations);
+  console.log(adjacentNodes);
+})
